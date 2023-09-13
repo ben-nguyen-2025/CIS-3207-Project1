c7af4e9480ad1691d983de0678b57120f451cb08
ben-nguyen-2025 <tuo53945@temple.edu>
2023-09-12 22:56:37 -0400

    added in no argument

--- tuls.c
+++ tuls.c
@@ -3,8 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-int main() {
-    puts("hi");
+int main(int argc, char *argv[]) {
+    struct dirent **namelist;
+    int n;
+    //from pdf of project part A
+    n=scandir(".",&namelist,NULL, alphasort);
+    if (n == -1) {
+        perror("scandir");
+        exit(EXIT_FAILURE);
+    }
+    while (n--)
+    {
+        printf("%s\n",namelist[n] -> d_name);
+        free(namelist[n]);
+    }
+
+    
+    
+
     return 0;
 }
